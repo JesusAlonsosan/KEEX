297343d45cada0bcbaca773bb545f66ab6f50adf
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-06-01 12:43:50 -0500

    multer :heavy_plus_sign:

---
+++ api/helpers/auth.js
@@ -0,0 +1,32 @@
+conts jwt = require("jesonwebtoken");
+const User = require("../models/User");
+
+exports.verifyToken = (req, res, next) => {
+  const {authorization: token } = req.headers;
+
+  jwt.verify(token, process.env.SECRET, (error,decoded) => {
+    if (error)
+    return res.status(401).json({
+      error,
+      message: "El Token ya ha expirado",
+    });
+    User.findById(decoded.id).then(user =>{
+      req.user = user;
+      next();
+    });
+  });
+};
+
+
+export.hasPermission = checkRoles = roles => {
+  return (req,res, next) => {
+    if (req.user && roles.includes (req.user.role)) {
+      return next();
+    } else {
+      res.status(403).json({
+        error:{},
+        message:"No tienes permiso",
+      });
+    }
+  };
+};
---
+++ api/helpers/multer.js
@@ -0,0 +1,21 @@
+const cloudinary = require("cloudinary");
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+
+cloundinary.config({
+  cloud_name:process.env.CLOUD,
+  api_key:process.env.APIKEY;
+  api_secret: process.env.APISECRET,
+});
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "todos", //El nombre del folder en cloudinary
+  allowedFormats: ["jpg", "jpeg", "png"],
+  filename: function(req, file, cb) {
+    cb(null, file.originalname); //Este documento en cloudinary tendria el mismo nombre
+  }
+});
+
+module.exports = multer({ storage });
