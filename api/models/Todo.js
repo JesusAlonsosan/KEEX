936e3ac2e737a8d663af802e1e4deb29ebc2f4a6
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-06-01 15:16:43 -0500

    models :construction:

---
+++ api/models/Todo.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema(
+  {
+    author: {
+      tyoe : Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    body: {
+      required: true,
+      type: String,
+    },
+    expiration_date: {
+      type: Date,
+      default: Date.now() + 86400000,
+    },
+    images: {
+      type: [String],
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "low"
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
