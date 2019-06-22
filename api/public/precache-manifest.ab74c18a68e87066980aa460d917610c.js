a9ef1a30524585b8f23737f3a6c0f6e10d51885d
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-06-22 00:55:12 -0500

    Ready use Heroku

---
+++ api/public/precache-manifest.ab74c18a68e87066980aa460d917610c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fa127471c97db36e8375a3bc23bd72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd7352c35a53a5712323",
+    "url": "/static/css/2.858b5ee9.chunk.css"
+  },
+  {
+    "revision": "d4c099cbde5545aa4e95",
+    "url": "/static/css/main.de79f4f3.chunk.css"
+  },
+  {
+    "revision": "bd7352c35a53a5712323",
+    "url": "/static/js/2.458314f5.chunk.js"
+  },
+  {
+    "revision": "d4c099cbde5545aa4e95",
+    "url": "/static/js/main.9744ec93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfe96790c547bce4cfd426f9a113a7b4",
+    "url": "/static/media/1.cfe96790.jpeg"
+  },
+  {
+    "revision": "3b6060fa534e27497bb7e6b30732c6a0",
+    "url": "/static/media/2.3b6060fa.jpeg"
+  },
+  {
+    "revision": "adc479a6acfa41ab771e4e39a135d275",
+    "url": "/static/media/3.adc479a6.jpeg"
+  },
+  {
+    "revision": "8e37e18b189541396a1cecf485a845b5",
+    "url": "/static/media/4.8e37e18b.jpeg"
+  },
+  {
+    "revision": "65ba0636f47c3098516aed3f069a717e",
+    "url": "/static/media/atico.65ba0636.jpeg"
+  },
+  {
+    "revision": "d508f581a350afd68ad142c17885c4c6",
+    "url": "/static/media/fondolending.d508f581.jpg"
+  },
+  {
+    "revision": "7a98f61121cb72ad445c25120fede3eb",
+    "url": "/static/media/logo2.7a98f611.png"
+  }
+]);
