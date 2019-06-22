5d6f466acc86d1b4be09033e0367b8dd499a2aae
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-06-21 20:21:21 -0500

    :pencil: swipe

--- front/src/components/Swipecontainer/Swipe.js
+++ front/src/components/Swipecontainer/Swipe.js
@@ -4,23 +4,13 @@ import { Link } from "react-router-dom";
 
 const Swipe = ({products}) => (
     
-    <div className="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 7:3; animation: push" >
+    <div className="uk-flex uk-flex-center" tabindex="-1" uk-slideshow="ratio: 7:3; animation: push" >
 
-    <ul className="uk-slideshow-items uk-flex uk-flex-center">
-     {products.map((product,i)=>
-                <li key={i}>
-                <img src={product.image} alt="" uk-cover/>
-                <div className="uk-overlay uk-overlay-primary uk-position-bottom uk-text-center uk-transition-slide-bottom">
-                    <h3 className="uk-margin-remove">{product.name}</h3>
-                    <p className="uk-margin-remove">{product.description}</p>
-                </div>
-            </li>
-            
-    )}
-    </ul>
+    <button className="uk-button uk-button-primary" uk-icon="icon: heart">Te gusta</button>
 
-    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
+      <img src="http://idata.over-blog.com/0/36/82/48/ONVNI/Sirene01.jpg" />
+
+    <button className="uk-button uk-button-danger" uk-icon="icon: close">No te gusta</button>
 
 </div>
 
